3a7f3c6079d7cef8db910b2665b08c702c85b48d
RafatMeraz <razhossain201@gmail.com>
2023-09-12 01:27:01 +0600

    verify otp screen ui in progress

--- lib/features/auth/presentation/ui/screen/verify_otp_screen.dart
+++ lib/features/auth/presentation/ui/screen/verify_otp_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:zenutri_app/common/presentation/utils/app_colors.dart';
 import 'package:zenutri_app/common/presentation/utils/spacing.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:zenutri_app/core/extensions/size_extension.dart';
 
 class VerifyOtpScreen extends StatefulWidget {
   const VerifyOtpScreen({super.key});
@@ -10,6 +12,8 @@ class VerifyOtpScreen extends StatefulWidget {
 }
 
 class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
+  final TextEditingController _otpTEController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +33,33 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
                 ),
               ),
               verticalSpace(24),
+              PinCodeTextField(
+                length: 4,
+                obscureText: false,
+                animationType: AnimationType.fade,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                keyboardType: TextInputType.number,
+                controller: _otpTEController,
+                pinTheme: PinTheme(
+                  shape: PinCodeFieldShape.box,
+                  borderRadius: BorderRadius.circular(50.rSp),
+                  fieldHeight: 45.rSp,
+                  fieldWidth: 60.rSp,
+                  activeFillColor: Colors.white,
+                  inactiveFillColor: Colors.white,
+                  selectedFillColor: Colors.white,
+                  inactiveColor: AppColors.manatee,
+                  activeColor: AppColors.primaryColor,
+                ),
+                animationDuration: const Duration(milliseconds: 300),
+                backgroundColor: Colors.white,
+                enableActiveFill: true,
+                beforeTextPaste: (text) {
+                  return true;
+                }, appContext: context,
+              ),
+              verticalSpace(24),
+
             ],
           ),
         ),
